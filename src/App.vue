eaceda2ab52bce9626b7e10141d041be5bde4912
yuu-eguci <yuu.eguci@gmail.com>
2022-05-24 16:40:49 +0900

    ✨ ざっくりとデータを表示
    参考: https://fullcalendar.io/docs/timeline-standard-view-demo

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,50 @@ export default {
     return {
       calendarOptions: {
         plugins: [ resourceTimelinePlugin ],
-        initialView: 'resourceTimeline',
+        initialView: 'resourceTimelineDay',
         locale: jaLocale,
+        resourceAreaColumns: [
+          {
+            field: 'name',
+            headerContent: '名前'
+          }
+        ],
+        resources: [
+          {
+            id: 1,
+            name: '芥川賞作家 砂川文次'
+          },
+          {
+            id: 2,
+            name: '芥川賞作家 石沢麻依',
+            eventColor: 'green'
+          },
+          {
+            id: 3,
+            name: '芥川賞作家 李琴峰',
+            eventColor: 'orange'
+          }
+        ],
+        events: [
+          {
+            resourceId: 1,
+            title: 'event 1',
+            start: '2022-05-23',
+            end: '2022-05-25'
+          },
+          {
+            resourceId: 2,
+            title: 'event 3',
+            start: '2022-05-24T12:00:00+00:00',
+            end: '2022-05-25T06:00:00+00:00'
+          },
+          {
+            resourceId: 3,
+            title: 'event 4',
+            start: '2022-05-24T07:30:00+00:00',
+            end: '2022-05-24T09:30:00+00:00'
+          }
+        ],
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives'
       }
     }
